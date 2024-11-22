cad0ff712d87a504c472fed3eda3863bfa70b59a
Lucas Teles <lucasouzateles@gmail.com>
2024-11-22 15:01:48 -0300

    feat: adicionando lógica do carrinho

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Bem vindo ao meu carrinho !");
+import * as cartService from './services/cart.js'
+import createItem from "./services/item.js";
+
+
+const myCart = [];
+const myWishList = [];
+
+console.log("Bem vindo ao seu carrinho !")
+
+const item1 = await createItem("Mouse Redragon", 20.99, 1);
+const item2 = await createItem("Pen drive 64GB", 64.20, 4);
+
+
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, item2);
+
+await cartService.displayCart(myCart);
+
+console.log("----- TOTAL -----")
+await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,15 +1,49 @@
-async function addItem(userCart) {
-    
+async function addItem(userCart, item) {
+    userCart.push(item);
 }
 
 async function deleteItem(userCart, name) {
-    
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if(index !== -1){
+        userCart.splice(index, 1);
+    }
 }
 
-async function removeItem(userCart, index) {
-    
+async function removeItem(userCart, item) {
+    const indexFound = userCart.findIndex((p) => p.name === item.name);
+    if(indexFound == -1){
+        console.log("item não encontrado");
+        return;
+    }
+
+    if(userCart[indexFound].quantity > 1){
+        userCart[indexFound].quantity -= 1;
+        return;
+    }
+
+    if(userCart[indexFound].quantity == 1){
+        userCart.splice(indexFound, 1);
+        return;
+    }
 }
 
 async function calculateTotal(userCart) {
-    
+    const result = userCart.reduce((total, item)=> total + item.subtotal(), 0);
+    console.log(`\nTotal: R$ ${result}`)
 }
+
+async function displayCart(userCart) {
+    console.log("Lista do carrinho");
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} -- R$ ${item.price} | ${item.quantity} | Subtotal R$ ${item.subtotal()}`);
+    });
+}
+
+export{
+    addItem,
+    removeItem,
+    deleteItem,
+    calculateTotal,
+    displayCart,
+};
